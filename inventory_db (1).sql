4efa316718d795f3572584a435fa652e5c66ce16
Rifkyrf <142363295+Rifkyrf@users.noreply.github.com>
2026-01-08 14:07:46 +0700

    tambah database
    database

--- inventory_db (1).sql
+++ inventory_db (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 07, 2026 at 09:34 AM
+-- Generation Time: Jan 08, 2026 at 08:08 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -38,8 +38,7 @@ CREATE TABLE `barang` (
 --
 
 INSERT INTO `barang` (`id`, `nama_barang`, `stok`) VALUES
-(3, 'laptop', 2),
-(4, 'pc', 0);
+(1, 'pc', 9);
 
 -- --------------------------------------------------------
 
@@ -59,35 +58,8 @@ CREATE TABLE `riwayat_aksi` (
 --
 
 INSERT INTO `riwayat_aksi` (`id`, `aksi`, `user_email`, `tanggal`) VALUES
-(1, 'Menambah barang baru: produk kosmetik', 'admin@mail.com', '2026-01-07 07:11:48'),
-(2, 'Menambah barang: playgame', 'admin@mail.com', '2026-01-07 07:15:32'),
-(3, 'Menambah barang: buku', 'admin@mail.com', '2026-01-07 07:16:17'),
-(4, 'Mencatat barang masuk: 12 unit pada ID: 6', 'admin@mail.com', '2026-01-07 07:16:21'),
-(5, 'Mencatat barang keluar: 16 unit pada ID: 6', 'admin@mail.com', '2026-01-07 07:16:28'),
-(6, 'Menambah barang: pc', 'admin@mail.com', '2026-01-07 07:17:50'),
-(7, 'Berhasil masuk: 24 unit pc', 'admin@mail.com', '2026-01-07 07:17:54'),
-(8, 'Berhasil keluar: 20 unit pc', 'admin@mail.com', '2026-01-07 07:18:00'),
-(9, 'Berhasil masuk: 7 unit pc', 'admin@mail.com', '2026-01-07 07:22:28'),
-(10, 'Menambah barang baru: no js', 'admin@mail.com', '2026-01-07 07:24:59'),
-(11, 'Berhasil masuk: 13 unit no js', 'admin@mail.com', '2026-01-07 07:25:03'),
-(12, 'Berhasil keluar: 13 unit no js', 'admin@mail.com', '2026-01-07 07:25:17'),
-(13, 'Menambah barang baru: motor', 'admin@mail.com', '2026-01-07 07:28:06'),
-(14, 'Berhasil masuk: 19000000000 unit motor', 'admin@mail.com', '2026-01-07 07:28:22'),
-(15, 'Menambah barang baru: kureng', 'admin@mail.com', '2026-01-07 07:30:09'),
-(16, 'Berhasil masuk: 89 unit kureng', 'admin@mail.com', '2026-01-07 07:30:34'),
-(17, 'Berhasil keluar: 78 unit kureng', 'admin@mail.com', '2026-01-07 07:30:51'),
-(18, 'Menambah barang baru: motor', 'admin@mail.com', '2026-01-07 07:45:27'),
-(19, 'Berhasil masuk: 10 unit motor', 'admin@mail.com', '2026-01-07 07:45:33'),
-(20, 'Berhasil keluar: 9 unit motor', 'admin@mail.com', '2026-01-07 07:45:41'),
-(21, 'Menambah barang baru: laptop', 'admin@mail.com', '2026-01-07 07:59:44'),
-(22, 'Berhasil masuk: 11 unit laptop', 'admin@mail.com', '2026-01-07 07:59:49'),
-(23, 'Berhasil keluar: 11 unit laptop', 'admin@mail.com', '2026-01-07 07:59:55'),
-(24, 'Berhasil masuk: 56 unit laptop', 'admin@mail.com', '2026-01-07 08:00:10'),
-(25, 'Berhasil masuk: 1000 unit laptop', 'admin@mail.com', '2026-01-07 08:00:23'),
-(26, 'Berhasil keluar: 1054 unit laptop', 'admin@mail.com', '2026-01-07 08:00:34'),
-(27, 'Menambah barang baru: pc', 'admin@mail.com', '2026-01-07 08:33:14'),
-(28, 'Berhasil masuk: 5 unit pc', 'admin@mail.com', '2026-01-07 08:33:18'),
-(29, 'Berhasil keluar: 5 unit pc', 'admin@mail.com', '2026-01-07 08:33:25');
+(1, 'Menambah barang baru: pc', 'rifkyauliafirstiandy@gmail.com', '2026-01-08 06:38:06'),
+(2, 'Berhasil masuk: 9 unit pc', 'rifkyauliafirstiandy@gmail.com', '2026-01-08 06:38:13');
 
 -- --------------------------------------------------------
 
@@ -108,15 +80,7 @@ CREATE TABLE `transaksi` (
 --
 
 INSERT INTO `transaksi` (`id`, `barang_id`, `jenis`, `jumlah`, `tanggal`) VALUES
-(6, 8, 'masuk', 13, '2026-01-07 07:25:03'),
-(7, 8, 'keluar', 13, '2026-01-07 07:25:16'),
-(13, 3, 'masuk', 11, '2026-01-07 07:59:49'),
-(14, 3, 'keluar', 11, '2026-01-07 07:59:55'),
-(15, 3, 'masuk', 56, '2026-01-07 08:00:10'),
-(16, 3, 'masuk', 1000, '2026-01-07 08:00:23'),
-(17, 3, 'keluar', 1054, '2026-01-07 08:00:34'),
-(18, 4, 'masuk', 5, '2026-01-07 08:33:18'),
-(19, 4, 'keluar', 5, '2026-01-07 08:33:25');
+(1, 1, 'masuk', 9, '2026-01-08 06:38:13');
 
 -- --------------------------------------------------------
 
@@ -135,7 +99,9 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `email`, `password`) VALUES
-(1, 'admin@mail.com', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi');
+(1, 'rifkyauliafirstiandy@gmail.com', '$2y$10$U58UBOXgxkdaNSL3hvZHkOf/HH6MomUGuf5neM71GwswKlNArgZN2'),
+(3, 'bjww@gmail.com', '$2y$10$biTzsHdzTZwTWn3KFyCzh.PkAokgV09NuSSWSRcjq5ZwmBuHClRHi'),
+(4, 'pacul@culcul', '$2y$10$Z2kqNu4h7K5a4jrZTl/7H.BLqBb081.pDDj9DZ27eoYY3gr0/5igu');
 
 --
 -- Indexes for dumped tables
@@ -175,35 +141,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `riwayat_aksi`
 --
 ALTER TABLE `riwayat_aksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`barang_id`) REFERENCES `barang` (`id`);
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
